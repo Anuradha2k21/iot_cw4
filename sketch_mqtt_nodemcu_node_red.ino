2450f9fd53e5b405d967d98e60a9e6e5463fadea
Anuradha2k21 <61109105+Anuradha2k21@users.noreply.github.com>
2023-10-01 21:42:07 +0530

    added the second led

--- sketch_mqtt_nodemcu_node_red.ino
+++ sketch_mqtt_nodemcu_node_red.ino
@@ -4,13 +4,14 @@
 const char* ssid = "anuradha";
 const char* password = "dse24tr6";
 
-const char* mqtt_server = "91.121.93.94";   //  "test.mosquitto.org" ip address
+const char* mqtt_server = "91.121.93.94";  //  "test.mosquitto.org" ip address
 
 WiFiClient espClient22;
 PubSubClient client(espClient22);
 
 // Lamp - LED - GPIO 4 = D2 on ESP-12E NodeMCU board
-const int led = 4;
+const int led1 = 4;
+const int led2 = 12;
 
 void setup_wifi() {
   delay(10);
@@ -42,13 +43,23 @@ void callback(String topic, byte* message, unsigned int length) {
   }
   Serial.println();
 
-  if (topic == "room/light") {
+  if (topic == "room/light_1") {
     Serial.print("Changing Room Light to ");
     if (messageInfo == "on") {
-      digitalWrite(led, HIGH);
+      digitalWrite(led1, HIGH);
       Serial.print("On");
     } else if (messageInfo == "off") {
-      digitalWrite(led, LOW);
+      digitalWrite(led1, LOW);
+      Serial.print("Off");
+    }
+  }
+  if (topic == "room/light_2") {
+    Serial.print("Changing Room Light to ");
+    if (messageInfo == "on") {
+      digitalWrite(led2, HIGH);
+      Serial.print("On");
+    } else if (messageInfo == "off") {
+      digitalWrite(led2, LOW);
       Serial.print("Off");
     }
   }
@@ -61,7 +72,8 @@ void reconnect() {
 
     if (client.connect("ESP8266Client22")) {
       Serial.println("connected");
-      client.subscribe("room/light");
+      client.subscribe("room/light_1");
+      client.subscribe("room/light_2");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -72,7 +84,8 @@ void reconnect() {
 }
 
 void setup() {
-  pinMode(led, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
